93dfbfd1c13f6f671a6d761ad0d2f3435729a0d7
lip3k <lipny90@gmail.com>
2018-06-10 10:28:07 +0100

    Fix to headers

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('view engine', 'jade');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT");
     res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
