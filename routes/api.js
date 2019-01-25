bd5c907186b1496196ea7878aaadcd2daef3a769
lip3k <lipny90@gmail.com>
2019-01-25 19:50:59 +0000

    Dummy delay

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/ping', function(req, res, next) {
-    res.send({status: 'OK'});
+    setTimeout(() => {
+        res.send({status: 'OK'});
+    }, 1000);
 });
 
 module.exports = router;
